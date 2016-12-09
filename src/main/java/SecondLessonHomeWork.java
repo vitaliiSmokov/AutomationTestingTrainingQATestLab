8930caa3f867ab0fd12f9304247da284cd6dc05b
Vitalii Smokov <vitalik23@windowslive.com>
2016-12-09 22:05:59 +0200

    Добавлено ожидание результатов поиска

--- src/main/java/SecondLessonHomeWork.java
+++ src/main/java/SecondLessonHomeWork.java
@@ -2,6 +2,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 
@@ -14,8 +16,11 @@ public class SecondLessonHomeWork {
         WebElement searchBoxForm = driver.findElement(By.className("b_searchboxForm"));
         searchBoxForm.sendKeys("automation");
         driver.findElement(By.id("sb_form_go")).click();
+        WebDriverWait webDriverWait = new WebDriverWait(driver,5);
+        By locator = By.className("b_algo");
+        webDriverWait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(locator));
         System.out.println("Page title: " + driver.getTitle());
-        List <WebElement> searchResultTitles = driver.findElements(By.className("b_algo"));
+        List <WebElement> searchResultTitles = driver.findElements(locator);
         for (WebElement printResult:searchResultTitles){
             System.out.println(printResult.findElement(By.tagName("h2")).getText());
         }
