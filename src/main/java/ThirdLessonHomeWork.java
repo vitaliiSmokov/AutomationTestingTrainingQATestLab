4c4d1ee0d4c420d671f75d414f1df7a4ec7197ca
Vitalii Smokov <vitalik23@windowslive.com>
2016-12-17 17:51:55 +0200

    Минимальний рефакторинг)

--- src/main/java/ThirdLessonHomeWork.java
+++ src/main/java/ThirdLessonHomeWork.java
@@ -14,10 +14,7 @@ public class ThirdLessonHomeWork {
 //1. Открыть главную страницу поисковой системы Bing.
         driver.navigate().to("http://www.bing.com");
 //2. Перейти в раздел поиска изображений. Дождаться, что заголовок страницы имеет название “Лента изображений Bing.”
-        By menuItemLocator = By.xpath(".//*[@id='scpl1' and text() = 'Images']");
-        WebElement menuItemImage = driver.findElement(menuItemLocator);
-        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(menuItemLocator));
-        menuItemImage.click();
+        goToImageSearch(driver, wait);
         String expectedTitle = "Bing Image Feed";
         wait.until(ExpectedConditions.titleIs(expectedTitle));
 
@@ -29,44 +26,55 @@ public class ThirdLessonHomeWork {
         JavascriptExecutor javaScript = (JavascriptExecutor) driver;
         javaScript.executeScript("window.scrollTo(0, document.body.scrollHeight)");
         List<WebElement> imageElementsAfterScroll = driver.findElements(locatorOfImages);
-        verifyScroll(imageElementsBeforeScroll, imageElementsAfterScroll);
+        verifyScrolling(imageElementsBeforeScroll, imageElementsAfterScroll);
         javaScript.executeScript("window.scrollTo(0, 0)");
 
 //4. В поисковую строку ввести слово без последней буквы (например “automatio” вместо “automation”).
-// Дождаться появления слова целиком в выпадающем списке предложений.
-// Выбрать искомое слово и дождаться загрузки результатов поискового запроса.
-
         By searchBoxFormLocator = By.cssSelector(".b_searchboxForm>input#sb_form_q");
         WebElement searchBoxForm = driver.findElement(searchBoxFormLocator);
         wait.until(ExpectedConditions.visibilityOf(searchBoxForm));
         searchBoxForm.sendKeys("automatio");
+        By automationLocator = By.xpath("//div[@class = 'sa_tm']/strong[text() = 'n']");
+        // Дождаться появления слова целиком в выпадающем списке предложений.
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(automationLocator));
+        // Выбрать искомое слово и дождаться загрузки результатов поискового запроса.
+        driver.findElement(automationLocator).click();
+        By automationsImagesLocator = By.xpath("//div[@class = 'dg_u']");
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(automationsImagesLocator));
 
-        WebElement completed = driver.findElement(By.xpath("//div[@class ='sa_tm' and text() = 'automatio'] /strong[text() = 'n']"));
-        completed.click();
 
 //5. Установить фильтр Дата: “В прошлом месяце”. Дождаться обновления результатов.
-        driver.findElement(By.xpath(".//span[@class = 'ftrTB ftrP11' and text() = 'Date']")).click();
-        driver.findElement(By.xpath(".//span[@class= 'ftrP12' and text() = 'Past month']")).click();
-        By imagesLocator = By.xpath(".//div[@class='dg_u']");
-        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(imagesLocator));
+        setTheDateFilterLastMonth(driver);
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(automationsImagesLocator));
 
 //6. Нажать на первое изображение из результатов поиска. Дождаться перехода в режим слайд шоу.
-        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.className("dg_u")));
         Actions mouse = new Actions(driver);
         By firstImageLocator = By.xpath(".//*[@id='dg_c']/div[1]/div/div[1]/div/a/img");
         mouse.moveToElement(driver.findElement(firstImageLocator)).build().perform();
         driver.findElement(firstImageLocator).click();
 
-        driver.switchTo().frame("frame#OverlaylFrame.insightsOverlay");
+        driver.switchTo().frame("OverlayIFrame");
         wait.until(ExpectedConditions.presenceOfElementLocated(By.id("detail_film")));
+//        7. Выполнить переключение на следующее, предыдущее изображение. После переключения между изображениями необходимо дожидаться обновления очереди изображений для показа в нижней части окна слайд шоу.
+//        8. Нажать на отображаемое изображение в режиме слайд шоу и удостовериться, что картинка загрузилась в отдельной вкладке/окне.
 
         driver.quit();
     }
 
+    private static void goToImageSearch(WebDriver driver, WebDriverWait wait) {
+        By menuItemLocator = By.xpath(".//*[@id='scpl1' and text() = 'Images']");
+        WebElement menuItemImage = driver.findElement(menuItemLocator);
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(menuItemLocator));
+        menuItemImage.click();
+    }
+
+    private static void setTheDateFilterLastMonth(WebDriver driver) {
+        driver.findElement(By.xpath(".//span[@class = 'ftrTB ftrP11' and text() = 'Date']")).click();
+        driver.findElement(By.xpath(".//span[@class= 'ftrP12' and text() = 'Past month']")).click();
+    }
 
 
-
-    private static void verifyScroll(List<WebElement> imageElementsBeforeScroll, List<WebElement> imageElementsAfterScroll) {
+    private static void verifyScrolling(List<WebElement> imageElementsBeforeScroll, List<WebElement> imageElementsAfterScroll) {
         if (imageElementsBeforeScroll.size() < imageElementsAfterScroll.size())
             System.out.println("Scroll pass");
         else
@@ -74,13 +82,3 @@ public class ThirdLessonHomeWork {
     }
 
 }
-/*//div[@class='dg_u']//a[@href='/images/search?q=automation&view=detailv2&qft=+filterui%3aage-lt43200&id=C14D5148F195D5C6E7E91796084BF89E7E512E75&selectedIndex=0&ccid=BUGpBwXT&simid=608051698326242586&thid=OIP.M0541a90705d3f4468df5ab2bda47a472o0']
-1. Открыть главную страницу поисковой системы Bing.
-2. Перейти в раздел поиска изображений. Дождаться, что заголовок страницы имеет название “Лента изображений Bing.”
-3. Выполнить прокрутку страницы несколько раз. Каждый раз проверять, что при достижении низа страницы подгружаются новые блоки с изображениями.
-4. В поисковую строку ввести слово без последней буквы (например “automatio” вместо “automation”). Дождаться появления слова целиком в выпадающем списке предложений. Выбрать искомое слово и дождаться загрузки результатов поискового запроса.
-5. Установить фильтр Дата: “В прошлом месяце”. Дождаться обновления результатов.
-6. Нажать на первое изображение из результатов поиска. Дождаться перехода в режим слайд шоу.
-7. Выполнить переключение на следующее, предыдущее изображение. После переключения между изображениями необходимо дожидаться обновления очереди изображений для показа в нижней части окна слайд шоу.
-8. Нажать на отображаемое изображение в режиме слайд шоу и удостовериться, что картинка загрузилась в отдельной вкладке/окне.
- */
